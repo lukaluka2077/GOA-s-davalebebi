b8f67376ed66cfccdeeaabc0b5eb50e8c73a34dc
lukaluka2077 <dzneladzeluka25@gmail.com>
2026-03-20 23:46:41 +0400

    second commit

---
+++ LEVEL 006/cw.py
@@ -0,0 +1,11 @@
+name=input("enter your name:")
+print(name)
+nummber=int(input("enter your number :" ) )
+print(nummber+19)
+number=int(input("enter your number :" ) )
+print(nummber+11)
+name=input("enter your name here:")
+print(name)
+float=6.8
+integer=15
+string="home"
---
+++ LEVEL 006/hw.py
@@ -0,0 +1,58 @@
+
+#2) კომენტარების სახით ახსენით რა არის input-ი და output-ი, მოიყავნეთ შესაბამისი მაგალითები.
+
+#3) შექმენით ცვლადი, რომელშიც შეინხავთ input ინსტრუქციით შემოტანილ მნიშვნელობას, შემდეგ შეამოწმებთ თუ რა ტიპის მონაცემი ინახება ამ ცვლადში და დაპრინტავთ.
+
+#4) თიოთეული მონაცემთა ტიპისთვის (str,int,float), შექმენით 5 ცვლადი და დაუწერეთ კომენტარი თუ რომელ მონაცემთა ტიპს ინახავს ცვლადი.
+
+#5) აიღეთ 3 ცვლადი, შეინახეთ განსხავებული მონაცემთა ტიპები (str,int,float), შემდეგ type ინსტრუქციის გამოყენებით შეამოწმეთ, თუ რომელ მონაცემთა ტიპს ინახავს ცვლადი.
+
+#6) მომხმარებელს შემოატანინეთ ორი სიტყვა, შეინახეთ ისინი ცვლადებში, მოახდინეთ მათი კონკატინაცია და დაბეჭდეთ.
+
+#7) მომხმარებელს შემოატანინეთ 5 რიცხვი სხვადასხვა დამოუკიდებელი input-ების სახით, თქვენი დავალებაა დაბეჭდოთ მათი საშუალო არითმეტიკული.
+
+#8) მომხმარებელს შემოატანინეთ სახელი, გვარი, ასაკი, სიმაღლე, წონა და ამ მონაცემების გამოყენებით დაბეჭდეთ ერთი დიდი წინადადება.
+
+#2)input-არის მაგალითად და მოკლედ რომ ვთქვათ ნივთი რომელლსაც ვმართავთ და ვაკონთროლებთ
+#output-არის რასაც ჩვენით ვერ ვაკონტროლებთ მაგალითად დინამიკი მონიტორი მაგრამ პითონში მას სულ სხვა ახსნა აქვს 
+
+#3)
+name=input("enter your name:")
+print(name)
+
+#4)
+string="hello world"
+print(string)
+integer=15
+print(integer)
+integer2=114
+print(integer2)
+float=12.1
+print(float)
+string2="goodbye"
+print(string2)
+
+#5)
+
+#7)
+num1=input("enter your nummber:")
+print(num1)
+num2=input("enter your nummber:")
+print(num2)
+num3=input("enter your nummber:")
+print(num3)
+num4=input("enter your nummber:")
+print(num4)
+print(num1 + num2+num3+num4 / 5)
+
+#7)
+name=input("enter your name:")
+print(name)
+last_name=input("enter your last name:")
+print(last_name)
+age=input("enter your age:")
+print(age)
+height=input("enter your height:")
+print(height)
+weight=input("enter your weight:")
+print(weight)
